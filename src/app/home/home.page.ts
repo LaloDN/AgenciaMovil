602870377a5ae01e290b995542700d3ab58b9533
LaloDN <lalorayado1@live.com.mx>
2020-11-27 22:17:03 -0600

    Ultimo Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  Sentra2020=[{id:1,name:"Sentra 2020 color naranja", img1:"assets/Nissan 1.png", img2:"assets/Nissan 2.png",img3:"assets/Nissan 3.png"}]
+
   constructor() {}
 
 }
